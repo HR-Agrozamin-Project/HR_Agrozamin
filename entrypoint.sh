ad7bd425953c62fccfad03ce8eb861309432cb5a
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-01-04 11:01:06 +0500

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+if [ "$DATABASE" = "HR_Agrozamin" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+sleep 10
+
+echo "Apply database migrations"
+python3 manage.py migrate
+
+
+echo "Starting server"
+python3 manage.py runserver --insecure 0.0.0.0:8000
+
+exec "$@"
