e1800bac0ddc5a9ee24a5b90b69350fb919ed6ae
Jobir Umirov <jobirumirovmoliya@gmail.com>
2023-01-13 22:57:55 +0500

    add command for compilemessages

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,9 @@ python3 manage.py migrate
 echo "initadmin"
 python3 create_admin.py
 
+echo "django-admin compilemessages`"
+django-admin compilemessages
+
 echo "Starting server"
 python3 manage.py runserver --insecure 0.0.0.0:8000
 
